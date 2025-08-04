7d398dcf08cf8b616c67a8ed7c6bb83525321ded
ihxnan <1377537238@qq.com>
2025-08-04 22:08:45 +0800

    commit ubuntu.sh

--- manjaro.sh
+++ manjaro.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
 # 更新
 pacman -Syyu
 
-# curl npm nodejs ranger neovim git 
-pacman -S curl npm nodejs ranger neovim git
+# curl npm nodejs ranger neovim git
+pacman -S curl npm nodejs ranger neovim i3
 
 mv alacritty ~/.config/
 mv nvim ~/.config/
 mv ranger ~/.config/
-mv .i3 ~/.config
+mv i3 ~/.config
---
+++ ubuntu.sh
@@ -0,0 +1,10 @@
+# 更新
+apt update
+
+apt install curl npm nodejs ranger neovim i3
+
+mv alacritty ~/.config/
+mv nvim ~/.config/
+mv ranger ~/.config/
+mv i3 ~/.config
+
