ee6c697c578cd579e6c80a853d15bf67ec6b534d
Ihxnan <1377537238@qq.com>
2025-09-18 11:25:11 +0800

    update

--- setup.sh
+++ setup.sh
@@ -9,6 +9,11 @@ sources_config=(
     .config/picom.conf
 )
 
+sources_home=(
+    .vimrc
+    .i3/
+)
+
 target_config=(
     ~/.config/alacritty/
     ~/.config/fish/
@@ -18,6 +23,13 @@ target_config=(
     ~/.config/picom.conf
 )
 
+target_home=(
+    ~/.vimrc
+    ~/.i3/
+)
+
 rm -rf "${target_config[@]}"
+rm -rf "${target_home[@]}"
 
 cp -r "${sources_config[@]}" ~/.config/
+cp -r "${sources_home[@]}" ~/
