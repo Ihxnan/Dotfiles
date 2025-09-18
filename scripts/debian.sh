6c9c61ba16b0bacab20ce4013e6d550c286b4b2d
Ihxnan <1377537238@qq.com>
2025-09-18 12:28:12 +0800

    update

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 sudo apt update
-sudo apt install i3-wm vim neovim alacritty fish i3blocks ranger picom
+sudo apt install i3-wm vim neovim alacritty fish i3blocks ranger picom npm gcc g++ gdb clangd nodejs yarnpkg
