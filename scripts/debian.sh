bec27209c08bd0cf840575f7e550db7ac87f682d
Ihxnan <1377537238@qq.com>
2025-09-18 13:18:04 +0800

    update

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
 
 sudo apt update
-sudo apt install i3-wm vim neovim alacritty fish i3blocks ranger picom npm gcc g++ gdb clangd nodejs yarnpkg
+
+sudo add-apt-repository ppa:neovim-ppa/unstable
+
+sudo apt update
+
+sudo apt install neovim
+
+sudo apt install git curl i3-wm vim alacritty fish i3blocks ranger picom npm gcc g++ gdb clangd nodejs yarnpkg
