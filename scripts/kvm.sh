8850461e2a6f542d546e3c022a07aca3edc0dbb9
ihxnan <1377537238@qq.com>
2025-10-07 20:34:04 +0800

    update

--- scripts/kvm.sh
+++ scripts/kvm.sh
@@ -2,15 +2,12 @@
 
 sudo pacman -Syu
 
-sudo pacman -S qemu-full edk2-ovmf dnsmasq vde2 bridge-utils openbsd-netcat swptm
-sudo pacman -S virt-manager virt-viewer libvirt libvirt-dbus ebtables iptables-nft
+sudo pacman -S qemu-full edk2-ovmf dnsmasq vde2 bridge-utils openbsd-netcat swptm virt-manager virt-viewer libvirt libvirt-dbus ebtables iptables-nft
 
 sudo systemctl enable --now libvirtd
 
 sudo usermod -aG libvirt $USER
 sudo usermod -aG kvm $USER
-
-sudo pacman -S bridge-utils
 
 nmcli connection add type bridge ifname br0 con-name br0
 nmcli connection add type bridge-slave ifname enp5s0 master br0
